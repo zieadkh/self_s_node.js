152b1575447699629f9ef7ea62a4583e0d9f721f
zieadkh <zyadk3503@gmail.com>
2025-12-22 09:36:46 +0200

    user sign out added so user can log in, out, delete, and even see list of all users!

--- src/controller/usercontroller.js
+++ src/controller/usercontroller.js
@@ -1,11 +1,12 @@
 import USER from "../modules/userInfo.js";
 import { genToken } from "../utils/generatetoken.js";
+import bcrypt from "bcrypt";
 
 export async function userSginUp (req, res ) {
   try {
     const {username, password, email} = req.body;
-    const exists = await USER.findOne({email: email});
-    if(exists) res.status(400).json({message: "user already exists!!"});
+    const userExists = await USER.findOne({email: email.toLowerCase()});
+    if(userExists) res.status(400).json({message: "user already exists!!"});
     const newUser = await USER.create({
       username,
       password,
@@ -20,6 +21,28 @@ export async function userSginUp (req, res ) {
   }
 }
 
+export async function signin (req, res) {
+  try {
+    const {email, password} = req.body;
+    const userExists = await USER.findOne({email: email.toLowerCase()});
+    if(!userExists) res.status(404).json({message: "incorrect email or password!"})
+    const passMatch = await bcrypt.compare(password, userExists.password);
+    if (passMatch) {
+      const token = genToken(userExists.id, res);
+      res.status(200).json({masssage: "user authenticated", token})
+    }
+  } catch (error) {
+    res.status(409).json({error: error.message});
+  }
+}
+
+export async function signout (req, res) {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+  res.status(200).json({message:"User looger OUT successfully!"});
+}
 
 export async function deleteUser (req, res) {
   try {
--- src/routers/inforouter.js
+++ src/routers/inforouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { allUsers, deleteUser, userSginUp } from '../controller/usercontroller.js';
+import { allUsers, deleteUser, signin, signout, userSginUp } from '../controller/usercontroller.js';
 
 export const router = express.Router();
 
 
 //router.post('/signin', signinInfo );
 router.post('/signup', userSginUp );
+router.post('/signin', signin );
+router.post('/signout', signout );
 router.delete('/delete/:id', deleteUser );
 router.get('/getall', allUsers);
 
